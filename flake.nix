5a591ad35048c83e1f5b65fca6263ea865d421a3
quotequack <quotequack+github@proton.me>
2025-09-29 18:46:14 +0300

    rewrote flake.nix config

--- flake.nix
+++ flake.nix
@@ -1,58 +1,49 @@
 {
-  description = "a silly keyboard sound program :D";
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    };
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
-
-  outputs = { self, nixpkgs, flake-utils }: 
-    flake-utils.lib.eachDefaultSystem (system:
-      let 
-        pkgs = import nixpkgs { inherit system; };
-        rust = pkgs.rustPlatform;
-
-      in {
-        packages.default = rust.buildRustPackage {
-          pname = "clickexla";
-          version = "0.1.0";
-          
-          src = ./.;
-
-          cargoLock = {
-            lockFile = ./Cargo.lock;
-          };
-          nativeBuildInputs = [ pkgs.libinput.dev pkgs.pkg-config ];
-          buildInputs = [ pkgs.cairo pkgs.gtk4 pkgs.alsa-lib pkgs.xdotool pkgs.xorg.libXi pkgs.gcc pkgs.libinput.dev pkgs.systemdMinimal pkgs.xorg.libX11.dev pkgs.xorg.libXtst ];
-          installPhase = ''
-            runHook preInstall
-            install -Dm755 target/release/clickexla $out/bin/clickexla
-            install -Dm644 settings.json $out/share/clickexla/settings.json
-            install -Dm644 icon.png $out/share/icons/hicolor/48x48/apps/clickexla.png
-            install -Dm644 clickexla.desktop $out/share/applications/clickexla.desktop
-            runHook postInstall
-          '';
+    outputs = { self, nixpkgs }: let
+    pkgs = nixpkgs.legacyPackages."x86_64-linux";
+    in {
+        packages."x86_64-linux".default = pkgs.rustPlatform.buildRustPackage {
+            name = "clickexla";
+            src = ./.;
+            buildInputs = [
+                pkgs.cairo 
+                pkgs.gtk4 
+                pkgs.alsa-lib 
+                pkgs.xdotool 
+                pkgs.xorg.libXi 
+                pkgs.gcc 
+                pkgs.libinput.dev 
+                pkgs.systemdMinimal 
+                pkgs.xorg.libX11.dev 
+                pkgs.xorg.libXtst
+                pkgs.rustc 
+                pkgs.cargo
+            ];
+            nativeBuildInputs = [ pkgs.pkg-config ];
+            cargoHash = "sha256-Q3ftrrX9ecDK/9Q5sGx5CDPw3vP+HUmuaHKHdc8w3Fo=";
         };
 
-        devShells.default = pkgs.mkShell {
-          buildInputs = [
-            pkgs.xdotool
-            pkgs.gcc
-            pkgs.cairo
-            pkgs.gtk4
-            pkgs.libinput.dev
-            pkgs.systemdMinimal
-            pkgs.xorg.libXtst
-            pkgs.xorg.libX11.dev
-            pkgs.pkg-config
-            pkgs.cargo
-            pkgs.alsa-lib
-            pkgs.rustc
-            pkgs.rust-analyzer
-            pkgs.xorg.libXi
-          ];
+        devShells."x86_64-linux".default= pkgs.mkShell {
+            buildInputs = [
+                pkgs.cairo 
+                pkgs.gtk4 
+                pkgs.alsa-lib 
+                pkgs.xdotool 
+                pkgs.xorg.libXi 
+                pkgs.gcc 
+                pkgs.libinput.dev 
+                pkgs.systemdMinimal 
+                pkgs.xorg.libX11.dev 
+                pkgs.xorg.libXtst
+                pkgs.rustc 
+                pkgs.cargo
+            ];
+            nativeBuildInputs = [ pkgs.pkg-config ];
+            env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
         };
-      }
-    );
+    };
 }
-
