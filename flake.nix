76ea6c295df29f82edd6d16d1a198fdf2679e967
quotequack <quotequack+github@proton.me>
2025-09-29 17:46:38 +0300

    Added desktop and default settings.json
    Preparing to make it a full on app

--- flake.nix
+++ flake.nix
@@ -24,6 +24,14 @@
           };
           nativeBuildInputs = [ pkgs.libinput.dev pkgs.pkg-config ];
           buildInputs = [ pkgs.cairo pkgs.gtk4 pkgs.alsa-lib pkgs.xdotool pkgs.xorg.libXi pkgs.gcc pkgs.libinput.dev pkgs.systemdMinimal pkgs.xorg.libX11.dev pkgs.xorg.libXtst ];
+          installPhase = ''
+            runHook preInstall
+            install -Dm755 target/release/clickexla $out/bin/clickexla
+            install -Dm644 settings.json $out/share/clickexla/settings.json
+            install -Dm644 icon.png $out/share/icons/hicolor/48x48/apps/clickexla.png
+            install -Dm644 clickexla.desktop $out/share/applications/clickexla.desktop
+            runHook postInstall
+          '';
         };
 
         devShells.default = pkgs.mkShell {
